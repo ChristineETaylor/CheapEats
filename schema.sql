44f27f24dbb2e75ce6ad39a8bcf6e767a014eed8
ChristineETaylor <christine.e.taylor.sf@gmail.com>
2017-10-05 11:03:29 -0700

    (fix) Updates price, name and Yelp API datatypes
    * Add 'price' fields to CheapItems and Deals tables
    * Update 'id' in YelpData table and foreign key 'yelp_ID' in CheapItems and Deals tables to reflect Yelp API's long/mixed character ids
    * Add restaurant 'name' field to YelpData table

--- schema.sql
+++ schema.sql
@@ -6,99 +6,97 @@
 -- SET FOREIGN_KEY_CHECKS=0;
 
 -- ---
--- Table 'Deals' -- 
+-- Table 'Deals'
+-- 
 -- ---
 
-DROP TABLE IF EXISTS Deals;
+DROP TABLE IF EXISTS `Deals`;
 		
-CREATE SEQUENCE Deals_seq;
-
-CREATE TABLE Deals (
-  id INTEGER NULL DEFAULT NEXTVAL ('Deals_seq') DEFAULT NULL,
-  yelp_ID INTEGER NULL DEFAULT NULL,
-  dealName VARCHAR(50) NOT NULL DEFAULT 'NULL',
-  description MEDIUMTEXT(250) NULL DEFAULT NULL,
-  imageURL VARCHAR(100) NOT NULL DEFAULT 'NULL',
-  startTime DATETIME(100) NOT NULL DEFAULT 'NULL',
-  endTime DATETIME(100) NOT NULL DEFAULT 'NULL',
-  PRIMARY KEY (id)
+CREATE TABLE `Deals` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `yelp_ID`  VARCHAR(100) NOT NULL DEFAULT NULL,
+  `price` DECIMAL(3, 2) NOT NULL DEFAULT NULL,
+  `dealName` VARCHAR(50) NOT NULL DEFAULT 'NULL',
+  `description` MEDIUMTEXT(250) NULL DEFAULT NULL,
+  `imageURL` VARCHAR(100) NOT NULL DEFAULT 'NULL',
+  `startTime` DATETIME(100) NOT NULL DEFAULT 'NULL',
+  `endTime` DATETIME(100) NOT NULL DEFAULT 'NULL',
+  PRIMARY KEY (`id`)
 );
 
 -- ---
--- Table 'CheapItems' -- 
+-- Table 'CheapItems'
+-- 
 -- ---
 
-DROP TABLE IF EXISTS CheapItems;
+DROP TABLE IF EXISTS `CheapItems`;
 		
-CREATE SEQUENCE CheapItems_seq;
-
-CREATE TABLE CheapItems (
-  id INTEGER NULL DEFAULT NEXTVAL ('CheapItems_seq') DEFAULT NULL,
-  yelp_ID INTEGER NULL DEFAULT NULL,
-  menuItem VARCHAR(100) NOT NULL DEFAULT 'NULL',
-  imageURL VARCHAR(100) NULL DEFAULT NULL,
-  description MEDIUMTEXT(250) NULL DEFAULT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE `CheapItems` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `yelp_ID` VARCHAR(100) NOT NULL DEFAULT NULL,
+  `price` DECIMAL(3, 2) NOT NULL DEFAULT NULL,
+  `menuItem` VARCHAR(100) NOT NULL DEFAULT 'NULL',
+  `imageURL` VARCHAR(100) NULL DEFAULT NULL,
+  `description` MEDIUMTEXT(250) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
 -- ---
--- Table 'Owners' -- 
+-- Table 'Owners'
+-- 
 -- ---
 
-DROP TABLE IF EXISTS Owners;
+DROP TABLE IF EXISTS `Owners`;
 		
-CREATE SEQUENCE Owners_seq;
-
-CREATE TABLE Owners (     
-  id INTEGER NULL DEFAULT NEXTVAL ('Owners_seq') DEFAULT NULL,
-  login CHAR(30) NOT NULL DEFAULT 'NULL',
-  password CHAR(60) NOT NULL DEFAULT 'NULL',
-  PRIMARY KEY (id)
+CREATE TABLE `Owners` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `login` CHAR(30) NOT NULL DEFAULT 'NULL',
+  `password` CHAR(60) NOT NULL DEFAULT 'NULL',
+  PRIMARY KEY (`id`)
 );
 
 -- ---
--- Table 'YelpData' -- 
+-- Table 'YelpData'
+-- 
 -- ---
 
-DROP TABLE IF EXISTS YelpData;
+DROP TABLE IF EXISTS `YelpData`;
 		
-CREATE SEQUENCE YelpData_seq;
-
-CREATE TABLE YelpData (
-  id INTEGER NULL DEFAULT NEXTVAL ('YelpData_seq') DEFAULT NULL,
-  address MEDIUMTEXT(50) NOT NULL DEFAULT 'NULL',
-  ZIP CAST(5 AS INT) NOT NULL DEFAULT NULL,
-  type VARCHAR(25) NOT NULL DEFAULT 'NULL',
-  imageURL VARCHAR(50) NULL DEFAULT NULL,
-  restaurantURL VARCHAR NULL DEFAULT NULL,
-  owner_ID INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE `YelpData` (
+  `id` VARCHAR(100) NOT NULL DEFAULT NULL,
+  `name` VARCHAR(100) NOT NULL DEFAULT NULL,
+  `address` MEDIUMTEXT(50) NOT NULL DEFAULT 'NULL',
+  `ZIP` INTEGER(5) NOT NULL DEFAULT NULL,
+  `type` VARCHAR(25) NOT NULL DEFAULT 'NULL',
+  `imageURL` VARCHAR(50) NULL DEFAULT NULL,
+  `restaurantURL` VARCHAR NULL DEFAULT NULL,
+  `owner_ID` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
 -- ---
--- Table 'Users' -- 
+-- Table 'Users'
+-- 
 -- ---
 
-DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS `Users`;
 		
-CREATE SEQUENCE Users_seq;
-
-CREATE TABLE Users (
-  id INTEGER NULL DEFAULT NEXTVAL ('Users_seq') DEFAULT NULL,
-  login VARCHAR(30) NOT NULL,
-  password VARCHAR(60) NOT NULL,
-  cheapitem_id INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE `Users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `login` VARCHAR(30) NOT NULL,
+  `password` VARCHAR(60) NOT NULL,
+  `cheapitem_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
 -- ---
 -- Foreign Keys 
 -- ---
 
-ALTER TABLE Deals ADD FOREIGN KEY (yelp_ID) REFERENCES YelpData (id);
-ALTER TABLE CheapItems ADD FOREIGN KEY (yelp_ID) REFERENCES YelpData (id);
-ALTER TABLE YelpData ADD FOREIGN KEY (owner_ID) REFERENCES Owners (id);
-ALTER TABLE Users ADD FOREIGN KEY (cheapitem_id) REFERENCES CheapItems (id);
+ALTER TABLE `Deals` ADD FOREIGN KEY (yelp_ID) REFERENCES `YelpData` (`id`);
+ALTER TABLE `CheapItems` ADD FOREIGN KEY (yelp_ID) REFERENCES `YelpData` (`id`);
+ALTER TABLE `YelpData` ADD FOREIGN KEY (owner_ID) REFERENCES `Owners` (`id`);
+ALTER TABLE `Users` ADD FOREIGN KEY (cheapitem_id) REFERENCES `CheapItems` (`id`);
 
 -- ---
 -- Table Properties
@@ -114,10 +112,10 @@ ALTER TABLE Users ADD FOREIGN KEY (cheapitem_id) REFERENCES CheapItems (id);
 -- Test Data
 -- ---
 
--- INSERT INTO `Deals` (`id`,`yelp_ID`,`dealName`,`description`,`imageURL`,`startTime`,`endTime`) VALUES
--- ('','','','','','','');
--- INSERT INTO `CheapItems` (`id`,`yelp_ID`,`menuItem`,`imageURL`,`description`) VALUES
--- ('','','','','');
+-- INSERT INTO `Deals` (`id`,`yelp_ID`,`price`,`dealName`,`description`,`imageURL`,`startTime`,`endTime`) VALUES
+-- ('','','','','','','','');
+-- INSERT INTO `CheapItems` (`id`,`yelp_ID`,`price`,`menuItem`,`imageURL`,`description`) VALUES
+-- ('','','','','','');
 -- INSERT INTO `Owners` (`id`,`login`,`password`) VALUES
 -- ('','','');
 -- INSERT INTO `YelpData` (`id`,`address`,`ZIP`,`type`,`imageURL`,`restaurantURL`,`owner_ID`) VALUES
